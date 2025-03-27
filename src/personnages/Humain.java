5c02cea75a366924624a8882111ec16f29a093b2
Pierrick Perdrieau <prp5546a@ensfsi.onmicrosoft.com>
2025-03-27 11:03:42 +0100

    TP4_Exo1 terminé

---
+++ src/personnages/Humain.java
@@ -0,0 +1,56 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonPref;
+	private int argent;
+	
+	public String getNom() {
+		return nom;
+	}
+	public int getArgent() {
+		return argent;
+	}
+	
+	public Humain(String nom, String boissonPref, int argent) {
+		super();
+		this.nom = nom;
+		this.boissonPref = boissonPref;
+		this.argent = argent;
+	}
+	
+	private void gagnerArgent(int gain) {
+		argent += gain;
+	}
+	
+	private boolean perdreArgent(int perte) {
+		if (argent < perte) {
+			return false;
+		}
+		else {
+			argent -= perte;
+			return true;
+		}
+	}
+	
+	private void parler(String texte) {
+		System.out.printf("(%s) - %s\n",nom,texte);
+	}
+	
+	public void direBonjour() {
+		parler(String.format("Bonjour ! Je m'appelle %s et j'aime boire du %s.",nom,boissonPref));
+	}
+	
+	public void boire() {
+		parler(String.format("Mmm, un bon verre de %s! GLOUPS !",boissonPref));
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (perdreArgent(prix)) {
+			parler(String.format("J'ai %d sous en poche. Je vais pouvoir m'offrir une %s à %d sous.",argent+prix,bien,prix));
+		}
+		else {
+			parler(String.format("Je n'ai plus que %d sous en poche. Je ne peux même pas m'offrir un %s à %d sous.",argent,bien,prix));
+		}
+	}
+}
