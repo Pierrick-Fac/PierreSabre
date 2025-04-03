261ca0a4871fd17cc0c3eb0eb1bc4d7abeb88b49
Pierrick-Fac <pierrick.perdrieau@univ-tlse3.fr>
2025-04-03 11:03:03 +0200

    Les humains ont des connaissances

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,9 +1,12 @@
 package personnages;
 
 public class Humain {
+	static final int TAILLE_MEMOIRE = 30;
 	private String nom;
 	private String boissonPref;
 	private int argent;
+	protected int nbConnaissance = 0;
+	protected Humain[] memoire = new Humain[TAILLE_MEMOIRE];
 	
 	public String getNom() {
 		return nom;
@@ -51,5 +54,37 @@ public class Humain {
 		else {
 			parler(String.format("Je n'ai plus que %d sous en poche. Je ne peux m�me pas m'offrir un %s � %d sous.",argent,bien,prix));
 		}
+	}
+	
+	public void faireConnaissanceAvec(Humain homme2) {
+		this.direBonjour();
+		homme2.repondre(this);
+		this.memoriser(homme2);
+	}
+	
+	private void repondre(Humain homme1) {
+		this.direBonjour();
+		this.memoriser(homme1);
+	}
+	
+	private void memoriser(Humain homme) {
+		if (nbConnaissance < TAILLE_MEMOIRE) {
+			memoire[nbConnaissance] = homme;
+			nbConnaissance++;
+		}
+		else {
+			for (int i = 0; i < nbConnaissance-1; i++) {
+				memoire[i] = memoire[i+1];
+			}
+			memoire[nbConnaissance-1] = homme;
+		}
+	}
+	
+	public void listerConnaissance() {
+		System.out.print("Je connais beaucoup de monde dont : ");
+		for (int i = 0; i < this.nbConnaissance; i++) {
+			System.out.print(memoire[i].nom + ", ");
+		}
+		System.out.println("");
 	}
 }
